553d09b9dcfd60adc9936d562e6b914879a6de3d
Proroc324 <misaleonidov851@gamil.com>
2023-11-20 17:39:15 +0300

    first commit

---
+++ app/src/main/java/ru/mggtk/music/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.mggtk.music
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Base_Theme_Music)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
