a25de750af63eaa207ae0e34b6c1f6873a3b8c20
Proroc324 <misaleonidov851@gamil.com>
2023-11-21 17:53:33 +0300

    4 commit

--- app/src/main/java/ru/mggtk/music/MainActivity.kt
+++ app/src/main/java/ru/mggtk/music/MainActivity.kt
@@ -3,13 +3,31 @@ package ru.mggtk.music
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_player_old)
+    }
+    var status: Boolean = false
+    fun btnStart(view: View){
+        val ib: ImageButton = findViewById(R.id.imageButton2)
+        if (status == false){
+            var anim = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_anim)
+            ib.setImageDrawable(anim)
+            anim?.start()
+            status = true
+        }
+        else {
+            var anim = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_anim2)
+            ib.setImageDrawable(anim)
+            anim?.start()
+            status = false
+        }
     }
 }
--- app/src/main/java/ru/mggtk/music/PlayerActivity.kt
+++ app/src/main/java/ru/mggtk/music/PlayerActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        setContentView(R.layout.activity_player_old)
     }
 }
